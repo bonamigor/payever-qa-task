4d7946bfbc09479394b2ff83a843c87d925df462
rafael.bonamigo <rafael.bonamigo@siagri.com.br>
2023-05-15 16:48:42 -0300

    Added first and last name to email options (faker)

--- src/AbstractCreateAccount.ts
+++ src/AbstractCreateAccount.ts
@@ -10,22 +10,24 @@ export abstract class AbstractPages {
 
   async createAccount() {
     const inputElements = await this.driver.wait(until.elementsLocated(By.css('div.label-container')), 10000);
-    const password = `Katun.${faker.date.anytime().getFullYear()}`
+    const password = `Katun.${faker.date.anytime().getFullYear()}`;
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
     for await (const element of inputElements) {
       await element.click();
       const input = (await element.getText()).trim()
 
       switch (input) {
         case 'First name':
-          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="firstName"]'))).sendKeys(faker.person.firstName());
+          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="firstName"]'))).sendKeys(firstName);
           break;
 
         case 'Last name':
-          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="lastName"]'))).sendKeys(faker.person.lastName());
+          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="lastName"]'))).sendKeys(lastName);
           break;
 
         case 'Email':
-          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="email"]'))).sendKeys(faker.internet.email());
+          await this.driver.wait(until.elementLocated(By.css('input[formcontrolname="email"]'))).sendKeys(faker.internet.email({ firstName, lastName }));
           break;
 
         case 'Password':
